77b384ddd7b998a322bc7b69a2f9c2940b0435a8
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-07-17 23:15:11 +0200

    :)

--- Job App/backend/routes/userRouter.js
+++ Job App/backend/routes/userRouter.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import { register } from '../controllers/userController.js'; 
 const router = express.Router();
 
-export default router
+router.post('/register', register);
+
+export default router;
